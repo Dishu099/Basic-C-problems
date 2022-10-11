750f12b28743526f8aea7f7eb44d3b128c5226f2
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-11 22:15:13 +0530

    Add files via upload

---
+++ STL in cpp/Inbuilt Sort in STL/SORT CODE WITH ARRAYS.cpp
@@ -0,0 +1,16 @@
+//SORT CODE WITH ARRAYS.cpp
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
