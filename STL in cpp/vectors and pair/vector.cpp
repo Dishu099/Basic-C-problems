e3225c06a774b448b79efac834cadf78d013e71a
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-10-12 23:14:20 +0530

    Add files via upload

---
+++ STL in cpp/vectors and pair/vector.cpp
@@ -0,0 +1,21 @@
+//vector.cpp
+#include<bits/stdc++.h>
+using namespace std;
+void printVec(vector<int> v){
+cout<<"size:"<<v.size()<<endl;
+for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+    }
+}
+int main(){
+    vector<int> v;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        printVec(v);
+        v.push_back(x);
+    }
+    printVec(v);
+}
