e89068c0f4544c0aca27f3cc1554f2157e1da38d
DishitaJain <97836714+Dishu099@users.noreply.github.com>
2022-11-29 17:08:39 +0530

    Add files via upload

---
+++ Recursive in c++/boardgame.cpp
@@ -0,0 +1,20 @@
+//count the no. of paths possible from start point to end point in gameboard.cpp
+#include<bits/stdc++.h>
+using namespace std;
+int countPath(int s, int e){
+    if(s==e){
+        return 1;
+    }
+    if(s>e){
+        return 0;
+    }
+    int count=0;
+    for(int i=1;i<=6;i++){
+        countPath(s+i,e);
+    }
+    return count;
+}
+int main(){
+    cout<<countPath(0,3)<<endl;
+    return 0;
+}
